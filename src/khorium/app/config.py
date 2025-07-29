3eb5aa1bf0780cb9972f5106d9645fedac2ef3c0
Leon Kwan <kwan.leon@gmail.com>
2025-07-28 22:44:04 -0700

    dotenv

--- src/khorium/app/config.py
+++ src/khorium/app/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 MESH_GENERATE_API = os.getenv("MESH_GENERATE_API", "http://localhost:10000/optimize_mesh")
 FRONTEND_URL = os.getenv("FRONTEND_URL", "https://www.app.khorium.ai")
